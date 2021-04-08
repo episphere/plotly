d18413cd2899a441520492e8a622a61f772def8e
Jonas Almeida <jalmeida@mathbiol.org>
2021-04-07 22:03:57 -0400

    preload plotly

--- epiPlotly.js
+++ epiPlotly.js
@@ -1,10 +1,27 @@
 console.log('epiPlotly.js loaded');
 
-(function(){
+(async function(){
+
+    loadScript=url=>new Promise(function(resolve, reject){
+        let s = document.createElement('script')
+        s.src=url
+        s.onload=resolve
+        document.head.appendChild(s)
+    })
+    
+    /*
+    loadScript=async url=>{
+        import(url)
+    }
+    */
+
+    if(typeof(Plotly)=="undefined"){
+        await loadScript('https://cdn.plot.ly/plotly-latest.min.js')
+    }
 
     class epiPlotly extends HTMLElement {
       connectedCallback() {
-        this.innerHTML = `<p><hr>Hello world from epiPlotly at ${Date()}<hr></p>`;
+        this.innerHTML = `<p><hr>Hello world from epiPlotly at ${Date()}<hr>Plotly = ${typeof(Plotly)}</p>`;
       }
     }
 
