eeb06413fd9f68aad72273b455b488b5890a16ca
Jonas Almeida <jalmeida@mathbiol.org>
2024-08-09 13:40:34 -0400

    typo

--- esm.mjs
+++ esm.mjs
@@ -1,5 +1,5 @@
 // usage:
-// Plotly = (await import('https://episphere.github.io/plotly/Plotly.mjs')).esm
+// Plotly = (await import('https://episphere.github.io/plotly/esm.mjs')).esm
 
 await import("https://cdn.plot.ly/plotly-2.34.0.min.js")
 const esm = Plotly
