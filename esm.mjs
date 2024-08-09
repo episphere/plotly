5b49c2324c96560113937976bce21a3c71c562f6
Jonas Almeida <jalmeida@mathbiol.org>
2024-08-09 13:34:23 -0400

    usage info

--- esm.mjs
+++ esm.mjs
@@ -1,3 +1,6 @@
+// usage:
+// Plotly = (await import('https://episphere.github.io/plotly/Plotly.mjs')).esm
+
 await import("https://cdn.plot.ly/plotly-2.34.0.min.js")
 const esm = Plotly
 export{esm}
